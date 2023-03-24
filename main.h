0bbff1e4cc31ff8886bb87fd471949cf8f9da3f0
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-24 15:40:40 -0700

    added main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+
+#endif /* MAIN_H */
+
