aecacc2f460cbe1389ac80410f17ec8e9e2718e9
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-03-27 02:29:48 -0700

    modified main.h

--- main.h
+++ main.h
@@ -3,13 +3,7 @@
 
 #include "stdlib.h"
 #include "stdio.h"
-#include "stdarg.h"
-
-/**
- * struct function_s - structure holding an identifier and a function
- * @identifier: character identifying action
- * @print_function: function excecuted when identifier is identified
-*/
+#include "stdarg"
 
 typedef struct function_s
 {
@@ -17,9 +11,13 @@ typedef struct function_s
 	int (*print_function)(va_list ap);
 } function_t;
 
-
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list ap);
+int printi(va_list ap);
+int print_int(int i);
+int print_string(va_list ap);
+int int_to_binary(int num);
+int int_to_bin(va_list ap);
 
-#endif /* MAIN_H */
-
+#endif /*_printf*/
